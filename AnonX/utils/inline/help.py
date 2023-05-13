c5f644af8cdaa0467da6988c08fb571c2a7ba9b0
CrezyDoll <126459834+CrezyDoll@users.noreply.github.com>
2023-05-13 21:22:43 +0530

    Update help.py

--- AnonX/utils/inline/help.py
+++ AnonX/utils/inline/help.py
@@ -14,9 +14,7 @@ def help_pannel(_, START: Union[bool, int] = None):
             text=_["BACK_BUTTON"],
             callback_data=f"settingsback_helper",
         ),
-        InlineKeyboardButton(
-            text=_["CLOSEMENU_BUTTON"], callback_data=f"close"
-        ),
+
     ]
     mark = second if START else first
     upl = InlineKeyboardMarkup(
@@ -90,10 +88,8 @@ def help_back_markup(_):
                 InlineKeyboardButton(
                     text=_["BACK_BUTTON"],
                     callback_data=f"settings_back_helper",
-                ),
-                InlineKeyboardButton(
-                    text=_["CLOSE_BUTTON"], callback_data=f"close"
                 )
+
             ]
         ]
     )
@@ -104,7 +100,7 @@ def private_help_panel(_):
     buttons = [
         [
             InlineKeyboardButton(
-                text="❄ ʜᴇʟᴩ ❄",
+                text="ʜᴇʟᴩ",
                 callback_data="settings_back_helper",
             ),
         ],
